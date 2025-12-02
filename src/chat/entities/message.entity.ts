6e314dbd2b31f8e6bd82bc65cb55b0e254ba15b9
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:25:59 -0300

    chore: update src/chat/entities/message.entity.ts

--- src/chat/entities/message.entity.ts
+++ src/chat/entities/message.entity.ts
@@ -12,23 +12,39 @@ export class Message implements PrismaMessage {
   @ApiProperty({ description: 'ID do remetente', example: 'uuid-do-remetente' })
   senderId: string;
 
-  @ApiProperty({ description: 'ID do destinatário', example: 'uuid-do-destinatario' })
+  @ApiProperty({
+    description: 'ID do destinatário',
+    example: 'uuid-do-destinatario',
+  })
   receiverId: string;
 
-  @ApiProperty({ description: 'Conteúdo da mensagem', example: 'Olá, tudo bem?' })
+  @ApiProperty({
+    description: 'Conteúdo da mensagem',
+    example: 'Olá, tudo bem?',
+  })
   content: string;
 
-  @ApiProperty({ description: 'Timestamp da mensagem', example: '2025-06-01T10:00:00.000Z' })
+  @ApiProperty({
+    description: 'Timestamp da mensagem',
+    example: '2025-06-01T10:00:00.000Z',
+  })
   timestamp: Date;
 
   @ApiProperty({ description: 'Status de leitura da mensagem', example: false })
   isRead: boolean;
 
   // Propriedades adicionadas para alinhar com o PrismaMessage
-  @ApiProperty({ description: 'Data e hora de criação da mensagem', example: '2025-06-01T10:00:00.000Z' })
+  @ApiProperty({
+    description: 'Data e hora de criação da mensagem',
+    example: '2025-06-01T10:00:00.000Z',
+  })
   createdAt: Date;
 
-  @ApiPropertyOptional({ description: 'URL de destino para a notificação associada à mensagem', example: '/app/messages/chatId', nullable: true })
+  @ApiPropertyOptional({
+    description: 'URL de destino para a notificação associada à mensagem',
+    example: '/app/messages/chatId',
+    nullable: true,
+  })
   targetUrl: string | null;
 
   // Propriedades de relação (não incluídas diretamente no construtor para simplicidade, mas presentes no tipo Prisma)
