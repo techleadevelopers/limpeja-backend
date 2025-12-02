839d2573fd356fe1f956e6964969ae1a7f0f7ae0
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:28:24 -0300

    chore: update src/ranking/ranking.controller.ts

--- src/ranking/ranking.controller.ts
+++ src/ranking/ranking.controller.ts
@@ -1,6 +1,11 @@
 // src/modules/ranking/ranking.controller.ts
 import { Controller, Get, Query, UseGuards, Param } from '@nestjs/common'; // Adicione 'Param' aqui
-import { ApiTags, ApiOperation, ApiQuery, ApiBearerAuth } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiQuery,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { RankingService } from './ranking.service';
 import { ProviderRankingDto } from './dto/provider-ranking.dto';
 import { SortByOption } from '../search/dto/search-query.dto';
@@ -15,11 +20,41 @@ export class RankingController {
 
   @Get('providers/local')
   @ApiOperation({ summary: 'Obter o ranking de provedores locais' })
-  @ApiQuery({ name: 'latitude', type: Number, description: 'Latitude da localização central', example: -23.55052, required: true })
-  @ApiQuery({ name: 'longitude', type: Number, description: 'Longitude da localização central', example: -46.63330, required: true })
-  @ApiQuery({ name: 'radius', type: Number, description: 'Raio de busca em KM (padrão: 10)', example: 5, required: false })
-  @ApiQuery({ name: 'sortBy', enum: SortByOption, description: 'Critério de ordenação (padrão: Rating)', example: SortByOption.Rating, required: false })
-  @ApiQuery({ name: 'limit', type: Number, description: 'Limite de resultados (padrão: 10)', example: 5, required: false })
+  @ApiQuery({
+    name: 'latitude',
+    type: Number,
+    description: 'Latitude da localização central',
+    example: -23.55052,
+    required: true,
+  })
+  @ApiQuery({
+    name: 'longitude',
+    type: Number,
+    description: 'Longitude da localização central',
+    example: -46.6333,
+    required: true,
+  })
+  @ApiQuery({
+    name: 'radius',
+    type: Number,
+    description: 'Raio de busca em KM (padrão: 10)',
+    example: 5,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'sortBy',
+    enum: SortByOption,
+    description: 'Critério de ordenação (padrão: Rating)',
+    example: SortByOption.Rating,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: Number,
+    description: 'Limite de resultados (padrão: 10)',
+    example: 5,
+    required: false,
+  })
   async getLocalProviderRanking(
     @Query('latitude') latitude: number,
     @Query('longitude') longitude: number,
@@ -27,15 +62,47 @@ export class RankingController {
     @Query('sortBy') sortBy?: SortByOption,
     @Query('limit') limit?: number,
   ): Promise<ProviderRankingDto[]> {
-    return this.rankingService.getProviderRanking(latitude, longitude, radius, sortBy, limit);
+    return this.rankingService.getProviderRanking(
+      latitude,
+      longitude,
+      radius,
+      sortBy,
+      limit,
+    );
   }
 
   @Get('providers/:providerId/position')
-  @ApiOperation({ summary: 'Obter a posição de um provedor específico no ranking local' })
-  @ApiQuery({ name: 'latitude', type: Number, description: 'Latitude da localização central', example: -23.55052, required: true })
-  @ApiQuery({ name: 'longitude', type: Number, description: 'Longitude da localização central', example: -46.63330, required: true })
-  @ApiQuery({ name: 'radius', type: Number, description: 'Raio de busca em KM (padrão: 10)', example: 5, required: false })
-  @ApiQuery({ name: 'sortBy', enum: SortByOption, description: 'Critério de ordenação (padrão: Rating)', example: SortByOption.Rating, required: false })
+  @ApiOperation({
+    summary: 'Obter a posição de um provedor específico no ranking local',
+  })
+  @ApiQuery({
+    name: 'latitude',
+    type: Number,
+    description: 'Latitude da localização central',
+    example: -23.55052,
+    required: true,
+  })
+  @ApiQuery({
+    name: 'longitude',
+    type: Number,
+    description: 'Longitude da localização central',
+    example: -46.6333,
+    required: true,
+  })
+  @ApiQuery({
+    name: 'radius',
+    type: Number,
+    description: 'Raio de busca em KM (padrão: 10)',
+    example: 5,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'sortBy',
+    enum: SortByOption,
+    description: 'Critério de ordenação (padrão: Rating)',
+    example: SortByOption.Rating,
+    required: false,
+  })
   async getProviderPosition(
     @Param('providerId') providerId: string, // Esta linha agora funcionará
     @Query('latitude') latitude: number,
@@ -43,6 +110,12 @@ export class RankingController {
     @Query('radius') radius?: number,
     @Query('sortBy') sortBy?: SortByOption,
   ) {
-    return this.rankingService.getProviderPositionInRanking(providerId, latitude, longitude, radius, sortBy);
+    return this.rankingService.getProviderPositionInRanking(
+      providerId,
+      latitude,
+      longitude,
+      radius,
+      sortBy,
+    );
   }
 }
