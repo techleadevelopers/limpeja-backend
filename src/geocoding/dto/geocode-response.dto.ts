be87019a01bb2b27939da6a1404866b127190d08
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:27:04 -0300

    chore: update src/geocoding/dto/geocode-response.dto.ts

--- src/geocoding/dto/geocode-response.dto.ts
+++ src/geocoding/dto/geocode-response.dto.ts
@@ -2,7 +2,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class GeocodeResponseDto {
-  @ApiProperty({ description: 'Latitude da localização', example: -23.550520 })
+  @ApiProperty({ description: 'Latitude da localização', example: -23.55052 })
   latitude: number;
 
   @ApiProperty({ description: 'Longitude da localização', example: -46.633308 })
