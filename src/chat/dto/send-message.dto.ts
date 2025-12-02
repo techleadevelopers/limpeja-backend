fdbfeb58d603749a25f69653cd80d2f9ab211b29
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:25:57 -0300

    chore: update src/chat/dto/send-message.dto.ts

--- src/chat/dto/send-message.dto.ts
+++ src/chat/dto/send-message.dto.ts
@@ -3,17 +3,26 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class SendMessageDto {
-  @ApiProperty({ description: 'ID da conversa à qual a mensagem pertence', example: 'uuid-da-conversa' })
+  @ApiProperty({
+    description: 'ID da conversa à qual a mensagem pertence',
+    example: 'uuid-da-conversa',
+  })
   @IsString()
   @IsNotEmpty()
   chatId: string;
 
-  @ApiProperty({ description: 'ID do destinatário da mensagem', example: 'uuid-do-destinatario' })
+  @ApiProperty({
+    description: 'ID do destinatário da mensagem',
+    example: 'uuid-do-destinatario',
+  })
   @IsString()
   @IsNotEmpty()
   receiverId: string; // O remetente será obtido do token JWT
 
-  @ApiProperty({ description: 'Conteúdo da mensagem', example: 'Preciso reagendar o serviço para amanhã.' })
+  @ApiProperty({
+    description: 'Conteúdo da mensagem',
+    example: 'Preciso reagendar o serviço para amanhã.',
+  })
   @IsString()
   @IsNotEmpty()
   content: string;
