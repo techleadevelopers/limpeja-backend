dc7912002cf8986f0b0b6586bece5b012ca3f96a
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:29:03 -0300

    chore: update src/test/test.controller.ts

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -6,16 +6,15 @@ import { main as seedDatabase } from '../../prisma/seed/seed'; // Ajuste o camin
 
 @Controller('test') // Isso define o prefixo da rota como /test
 export class TestController {
-  constructor(
-    // Se precisar de algum serviço no seed, injete aqui. Ex:
-    // private readonly prisma: PrismaService
-  ) {}
+  constructor() {} // private readonly prisma: PrismaService // Se precisar de algum serviço no seed, injete aqui. Ex:
 
   @Post('seed') // Isso define o endpoint completo como /test/seed
   async seed(@Res() res: Response) {
     // Guard-rail para garantir que não roda em produção
     if (process.env.NODE_ENV === 'production') {
-      return res.status(HttpStatus.FORBIDDEN).send('Seed operations are forbidden in production.');
+      return res
+        .status(HttpStatus.FORBIDDEN)
+        .send('Seed operations are forbidden in production.');
     }
 
     try {
@@ -25,7 +24,9 @@ export class TestController {
       return res.status(HttpStatus.OK).send('Database seeded successfully!');
     } catch (error) {
       console.error('Erro ao seedar o banco de dados via API:', error);
-      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Failed to seed database.');
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .send('Failed to seed database.');
     }
   }
 }
