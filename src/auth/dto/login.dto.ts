b6a58d760b01e24eeac75d9b0725874b038c4615
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:25:33 -0300

    chore: update src/auth/dto/login.dto.ts

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -3,12 +3,18 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger'; // <<-- IMPORTANTE: Adicionar esta importação
 
 export class LoginDto {
-  @ApiProperty({ description: 'Endereço de e-mail do usuário', example: 'usuario@example.com' }) // <<-- ADICIONADO
+  @ApiProperty({
+    description: 'Endereço de e-mail do usuário',
+    example: 'usuario@example.com',
+  }) // <<-- ADICIONADO
   @IsEmail({}, { message: 'O email deve ser um endereço de email válido.' })
   @IsNotEmpty({ message: 'O email é obrigatório.' })
   email: string;
 
-  @ApiProperty({ description: 'Senha do usuário', example: 'SuaSenhaSegura123!' }) // <<-- ADICIONADO
+  @ApiProperty({
+    description: 'Senha do usuário',
+    example: 'SuaSenhaSegura123!',
+  }) // <<-- ADICIONADO
   @IsString({ message: 'A senha deve ser uma string.' })
   @IsNotEmpty({ message: 'A senha é obrigatória.' })
   @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres.' })
