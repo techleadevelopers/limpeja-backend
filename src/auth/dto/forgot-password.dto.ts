68a4b8f9e81ddc2b472277f8b2e5cc78c13b4069
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:25:32 -0300

    chore: update src/auth/dto/forgot-password.dto.ts

--- src/auth/dto/forgot-password.dto.ts
+++ src/auth/dto/forgot-password.dto.ts
@@ -3,7 +3,10 @@ import { IsEmail, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ForgotPasswordDto {
-  @ApiProperty({ description: 'Endereço de e-mail para redefinição de senha', example: 'usuario@example.com' })
+  @ApiProperty({
+    description: 'Endereço de e-mail para redefinição de senha',
+    example: 'usuario@example.com',
+  })
   @IsEmail({}, { message: 'O email deve ser um endereço de email válido.' })
   @IsNotEmpty({ message: 'O email é obrigatório.' })
   email: string;
