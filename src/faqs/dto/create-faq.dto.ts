91a2628feee65c0419729f85990477589644bd21
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:26:55 -0300

    chore: update src/faqs/dto/create-faq.dto.ts

--- src/faqs/dto/create-faq.dto.ts
+++ src/faqs/dto/create-faq.dto.ts
@@ -3,17 +3,27 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsOptional, IsInt, Min } from 'class-validator';
 
 export class CreateFaqDto {
-  @ApiProperty({ description: 'A pergunta do FAQ', example: 'Como faço para agendar um serviço?' })
+  @ApiProperty({
+    description: 'A pergunta do FAQ',
+    example: 'Como faço para agendar um serviço?',
+  })
   @IsString()
   @IsNotEmpty()
   question: string;
 
-  @ApiProperty({ description: 'A resposta para a pergunta do FAQ', example: 'Você pode agendar um serviço diretamente pelo aplicativo, selecionando o tipo de serviço e o provedor.' })
+  @ApiProperty({
+    description: 'A resposta para a pergunta do FAQ',
+    example:
+      'Você pode agendar um serviço diretamente pelo aplicativo, selecionando o tipo de serviço e o provedor.',
+  })
   @IsString()
   @IsNotEmpty()
   answer: string;
 
-  @ApiPropertyOptional({ description: 'Categoria do FAQ (ex: Geral, Pagamentos, Serviços)', example: 'Geral' })
+  @ApiPropertyOptional({
+    description: 'Categoria do FAQ (ex: Geral, Pagamentos, Serviços)',
+    example: 'Geral',
+  })
   @IsOptional()
   @IsString()
   category?: string;
