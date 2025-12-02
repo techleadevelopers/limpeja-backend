17cea5399eeb66f12c3b86ad8edeaf02098779e1
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:26:46 -0300

    chore: update src/disputes/dto/update-dispute.dto.ts

--- src/disputes/dto/update-dispute.dto.ts
+++ src/disputes/dto/update-dispute.dto.ts
@@ -1,23 +1,47 @@
-import { IsEnum, IsString, IsNotEmpty, IsOptional, IsNumber, IsPositive } from 'class-validator';
+import {
+  IsEnum,
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsNumber,
+  IsPositive,
+} from 'class-validator';
 import { DisputeStatus } from '@prisma/client';
-import { ApiProperty, ApiPropertyOptional, OmitType, PartialType } from '@nestjs/swagger';
+import {
+  ApiProperty,
+  ApiPropertyOptional,
+  OmitType,
+  PartialType,
+} from '@nestjs/swagger';
 import { CreateDisputeDto } from './create-dispute.dto';
 
 // Use PartialType para tornar todos os campos de CreateDisputeDto opcionais
-export class UpdateDisputeDto extends PartialType(OmitType(CreateDisputeDto, ['bookingId'])) {
+export class UpdateDisputeDto extends PartialType(
+  OmitType(CreateDisputeDto, ['bookingId']),
+) {
   @IsEnum(DisputeStatus)
   @IsNotEmpty()
-  @ApiProperty({ enum: DisputeStatus, description: 'Novo status da disputa.', example: 'RESOLVED' })
+  @ApiProperty({
+    enum: DisputeStatus,
+    description: 'Novo status da disputa.',
+    example: 'RESOLVED',
+  })
   status: DisputeStatus;
 
   @IsOptional()
   @IsString()
-  @ApiPropertyOptional({ description: 'Notas de resolução (preenchido por um admin).', example: 'Disputa resolvida com reembolso parcial.' })
+  @ApiPropertyOptional({
+    description: 'Notas de resolução (preenchido por um admin).',
+    example: 'Disputa resolvida com reembolso parcial.',
+  })
   resolutionNotes?: string;
 
   @IsOptional()
   @IsNumber()
   @IsPositive()
-  @ApiPropertyOptional({ description: 'Valor do reembolso final a ser processado.', example: 25.00 })
+  @ApiPropertyOptional({
+    description: 'Valor do reembolso final a ser processado.',
+    example: 25.0,
+  })
   refundAmount?: number;
 }
