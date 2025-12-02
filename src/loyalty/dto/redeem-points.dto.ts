8b3a2a7d36226c61faba8b2473cc35735a9f2682
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:27:21 -0300

    chore: update src/loyalty/dto/redeem-points.dto.ts

--- src/loyalty/dto/redeem-points.dto.ts
+++ src/loyalty/dto/redeem-points.dto.ts
@@ -7,11 +7,17 @@ export class RedeemPointsDto {
   @IsUUID()
   rewardId: string;
 
-  @ApiProperty({ description: 'Quantidade de pontos a serem resgatados para esta recompensa.' })
+  @ApiProperty({
+    description:
+      'Quantidade de pontos a serem resgatados para esta recompensa.',
+  })
   @IsNumber()
   pointsToRedeem: number;
 
-  @ApiProperty({ description: 'Tipo da recompensa (e.g., DISCOUNT_COUPON, GIFT_CARD).', example: 'DISCOUNT_COUPON' })
+  @ApiProperty({
+    description: 'Tipo da recompensa (e.g., DISCOUNT_COUPON, GIFT_CARD).',
+    example: 'DISCOUNT_COUPON',
+  })
   @IsString()
   rewardType: string; // Pode ser um enum se você tiver tipos de recompensa fixos
 }
