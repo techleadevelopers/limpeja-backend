8a9276c5719d2cd3b6d5e6944db8b0062a768e2e
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:27:59 -0300

    chore: update src/prisma/prisma.service.ts

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,17 @@
 // src/prisma/prisma.service.ts
-import { INestApplication, Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     super({
       log: ['warn', 'error'], // opcional: ['query', 'info', 'warn', 'error']
