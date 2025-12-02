e7d3c0b09ac883a0e07811bbc7de0149d1e477d2
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-02 13:28:28 -0300

    chore: update src/reviews/dto/get-reviews.dto.ts

--- src/reviews/dto/get-reviews.dto.ts
+++ src/reviews/dto/get-reviews.dto.ts
@@ -3,17 +3,26 @@ import { IsString, IsOptional, IsInt, Min, Max } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class GetReviewsDto {
-  @ApiPropertyOptional({ description: 'Filtrar avaliações por ID do provedor', example: 'uuid-do-provedor' })
+  @ApiPropertyOptional({
+    description: 'Filtrar avaliações por ID do provedor',
+    example: 'uuid-do-provedor',
+  })
   @IsOptional()
   @IsString()
   providerId?: string;
 
-  @ApiPropertyOptional({ description: 'Filtrar avaliações por ID do cliente', example: 'uuid-do-cliente' })
+  @ApiPropertyOptional({
+    description: 'Filtrar avaliações por ID do cliente',
+    example: 'uuid-do-cliente',
+  })
   @IsOptional()
   @IsString()
   clientId?: string;
 
-  @ApiPropertyOptional({ description: 'Filtrar avaliações com pontuação mínima (1 a 5)', example: 4 })
+  @ApiPropertyOptional({
+    description: 'Filtrar avaliações com pontuação mínima (1 a 5)',
+    example: 4,
+  })
   @IsOptional()
   @Type(() => Number) // Garante que o valor seja tratado como número
   @IsInt()
@@ -21,7 +30,10 @@ export class GetReviewsDto {
   @Max(5)
   minRating?: number;
 
-  @ApiPropertyOptional({ description: 'Filtrar avaliações com pontuação máxima (1 a 5)', example: 5 })
+  @ApiPropertyOptional({
+    description: 'Filtrar avaliações com pontuação máxima (1 a 5)',
+    example: 5,
+  })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
