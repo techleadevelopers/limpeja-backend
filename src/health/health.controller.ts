5bdd037d7a17701f78ae358f25dfe54150769c1a
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-17 22:25:05 -0300

    chore: update src/health/health.controller.ts

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,8 +1,4 @@
-import {
-  Controller,
-  Get,
-  ServiceUnavailableException,
-} from '@nestjs/common';
+import { Controller, Get, ServiceUnavailableException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CacheService } from '../cache/cache.service';
 
