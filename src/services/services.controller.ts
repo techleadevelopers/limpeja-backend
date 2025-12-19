98fd63d6ab9fe7ab0a2641f24f8c974534c66200
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-19 09:42:52 -0300

    chore: update src/services/services.controller.ts

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -16,7 +16,7 @@ import { ServiceDetailsDto } from './dto/service-details.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
-import { UserRole, Service as PrismaServiceType } from '@prisma/client'; // <-- Importar Service do Prisma para tipagem
+import { UserRole } from '@prisma/client'; // <-- Importar Service do Prisma para tipagem
 import {
   ApiBearerAuth,
   ApiOperation,
