ad8c7eecfee0492ca761dd2a94ad2dec06dadc31
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-20 22:34:59 -0300

    chore: update src/src/offers/dto/update-offer.dto.ts

--- src/src/offers/dto/update-offer.dto.ts
+++
@@ -1,5 +0,0 @@
-// src/offers/dto/update-offer.dto.ts
-import { PartialType } from '@nestjs/swagger';
-import { CreateOfferDto } from './create-offer.dto';
-
-export class UpdateOfferDto extends PartialType(CreateOfferDto) {}
