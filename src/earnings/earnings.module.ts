c9fecf0055b5e1c9872e2fc0fe9bbb7178c9c954
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-20 23:03:29 -0300

    chore: update src/earnings/earnings.module.ts

--- src/earnings/earnings.module.ts
+++ src/earnings/earnings.module.ts
@@ -1,18 +1,18 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { EarningsController } from './earnings.controller';
 import { EarningsService } from './earnings.service';
-import { PrismaModule } from '../prisma/prisma.module'; // Já está aqui, o que é bom!
-import { ProvidersModule } from '../providers/providers.module'; // <<<< NOVO: Importe o ProvidersModule
+import { PrismaModule } from '../prisma/prisma.module';
+import { ProvidersModule } from '../providers/providers.module';
 import { PayoutsModule } from '../payouts/payouts.module';
 
 @Module({
   imports: [
     PrismaModule,
-    ProvidersModule, // <<<< Adicione ProvidersModule aqui
+    forwardRef(() => ProvidersModule),
     forwardRef(() => PayoutsModule),
   ],
   controllers: [EarningsController],
   providers: [EarningsService],
-  exports: [EarningsService], // Exporta o serviço caso outros módulos (ex: Dashboard) precisem dele
+  exports: [EarningsService],
 })
 export class EarningsModule {}
