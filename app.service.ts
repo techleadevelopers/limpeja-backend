3718adf0ec600e518fb06dc9a3917640f7237fd6
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-20 22:23:00 -0300

    chore: update app.service.ts

--- app.service.ts
+++
@@ -1,9 +0,0 @@
-// src/app.service.ts
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Bem-vindo à API Cleaning!';
-  }
-}
