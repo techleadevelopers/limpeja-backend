2c4cff3211ae71f36df46e90ca8a073a57222c3c
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-20 22:37:29 -0300

    chore: update src/verification/dto/upload-selfie.dto.ts

--- src/verification/dto/upload-selfie.dto.ts
+++
@@ -1,8 +0,0 @@
-// src/verification/dto/upload-selfie.dto.ts
-import { IsOptional, IsString } from 'class-validator';
-
-export class UploadSelfieDto {
-  // Similar ao UploadDocumentDto, o arquivo será tratado pelo @UploadedFile()
-  // No momento, não há campos adicionais necessários para a selfie além do arquivo.
-  // Poderíamos adicionar um campo para 'consentimento' se fosse um requisito.
-}
