45bb4840a7b6eb362651828d69c2e636ddb94ca3
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-20 22:36:39 -0300

    chore: update src/src/shared/types/user-roles.type.ts

--- src/src/shared/types/user-roles.type.ts
+++
@@ -1,4 +0,0 @@
-// src/shared/types/user-roles.type.ts
-import { UserRole } from '../../common/constants/roles.enum';
-
-export type UserRoles = UserRole.CLIENT | UserRole.PROVIDER | UserRole.ADMIN;
