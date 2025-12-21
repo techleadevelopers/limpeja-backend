7c56575398d74326a307762df2bcf0aac52b8708
devopscomputer <pauloti.engenharia@gmail.com>
2025-12-20 21:44:20 -0300

    chore: update src/providers/dto/provider-view.dto.ts

---
+++ src/providers/dto/provider-view.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { VerificationStatus } from '@prisma/client';
+import { ProviderWithCalculatedRating } from '../providers.service';
+import { ProviderDetailsDto } from './provider-details.dto';
+
+export class ProviderViewDto extends ProviderDetailsDto {
+  @ApiProperty({
+    description: 'Flag que indica se o provedor passou pelo processo de verificação.',
+    example: true,
+  })
+  isVerified: boolean;
+
+  constructor(source: ProviderWithCalculatedRating) {
+    super(source);
+    this.isVerified = source.verificationStatus === VerificationStatus.APPROVED;
+  }
+}
