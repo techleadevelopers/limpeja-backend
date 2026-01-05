c7d19cdc019e6347a9117f9b981e32c82485d1d1
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-05 00:19:30 -0300

    chore: update src/common/common.module.ts

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,14 +1,16 @@
 // src/common/common.module.ts
-import { Module, Global } from '@nestjs/common';
+import { Module, Global, forwardRef } from '@nestjs/common';
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 import { I18nService } from './i18n/i18n.service';
 import { APP_FILTER } from '@nestjs/core';
 import { LocaleMiddleware } from './middlewares/locale.middleware';
 import { ContactLeakDetector } from './services/contact-leak-detector.service';
 import { ContactLeakPolicyService } from './services/contact-leak-policy.service';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Global() // Torna este módulo globalmente disponível
 @Module({
+  imports: [forwardRef(() => NotificationsModule)],
   providers: [
     I18nService,
     {
