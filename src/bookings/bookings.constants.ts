c9d69861ed4efe2be17731d4e10bbcb4cacd58c3
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-06 17:17:57 -0300

    chore: update src/bookings/bookings.constants.ts

--- src/bookings/bookings.constants.ts
+++ src/bookings/bookings.constants.ts
@@ -2,6 +2,7 @@ import { BookingStatus } from '@prisma/client';
 
 export const BLOCKED_BOOKING_STATUSES: BookingStatus[] = [
   BookingStatus.PENDING,
+  BookingStatus.PENDING_PAYMENT,
   BookingStatus.PENDING_PROVIDER_CONFIRMATION,
   BookingStatus.CONFIRMED,
   BookingStatus.ON_THE_WAY,
