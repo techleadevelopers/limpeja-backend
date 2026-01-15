2cc9c53a50e3866d0ab742d915d1309c45f3cc73
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-15 14:27:14 -0300

    chore: update src/services/services.module.ts

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -2,6 +2,7 @@
 import { Module } from '@nestjs/common';
 import { ServicesService } from './services.service';
 import { ServicesController } from './services.controller';
+import { NotificationService } from './NotificationService';
 
 // Importe o módulo de cache para que o ServicesService possa usá-lo
 import { CacheModule } from '../cache/cache.module';
@@ -13,7 +14,7 @@ import { PrismaModule } from '../prisma/prisma.module';
     CacheModule, // <-- Adicione CacheModule aqui para resolver a dependência
   ],
   controllers: [ServicesController],
-  providers: [ServicesService],
-  exports: [ServicesService],
+  providers: [ServicesService, NotificationService],
+  exports: [ServicesService, NotificationService],
 })
 export class ServicesModule {}
