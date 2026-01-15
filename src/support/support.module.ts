2789df49469d3fd8f45c13f11261934b89b2407d
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-15 16:02:29 -0300

    chore: update src/support/support.module.ts

--- src/support/support.module.ts
+++ src/support/support.module.ts
@@ -5,14 +5,14 @@ import { SupportController } from './support.controller';
 import { SupportService } from './support.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
-import { NotificationsModule } from '../notifications/notifications.module'; // Para notificar usuários
+import { ServicesModule } from '../services/services.module';
 import { QueuesModule } from '../queues/queues.module'; // Para jobs de escalonamento
 import { SupportSlaPolicy } from './policies/sla.policy';
 import { SupportStateMachine } from './states/support.state-machine';
 import { EscalationsJobProcessor } from './jobs/escalations.job';
 
 @Module({
-  imports: [PrismaModule, AuthModule, NotificationsModule, QueuesModule],
+  imports: [PrismaModule, AuthModule, ServicesModule, QueuesModule],
   controllers: [SupportController],
   providers: [
     SupportService,
