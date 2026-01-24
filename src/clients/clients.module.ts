28325861ce8c71b92b529e6cdf6b633a15e9fdbc
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 11:32:11 -0300

    chore: update src/clients/clients.module.ts

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -4,11 +4,13 @@ import { ClientsService } from './clients.service';
 import { UsersModule } from '../users/users.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ClientsController } from './clients.controller';
+import { GeocodingModule } from '../geocoding/geocoding.module';
 
 @Module({
   imports: [
     forwardRef(() => UsersModule), // CORREÇÃO: Adicionado forwardRef para resolver a dependência circular.
     PrismaModule,
+    GeocodingModule,
   ],
   controllers: [ClientsController],
   providers: [ClientsService],
