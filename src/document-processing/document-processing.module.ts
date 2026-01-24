489cd43f1b4b880c5719a094b731acdf87cb4849
devopscomputer <pauloti.engenharia@gmail.com>
2026-01-24 11:32:15 -0300

    chore: update src/document-processing/document-processing.module.ts

--- src/document-processing/document-processing.module.ts
+++ src/document-processing/document-processing.module.ts
@@ -2,11 +2,10 @@
 import { Module } from '@nestjs/common';
 import { UploadModule } from '../upload/upload.module';
 import { DocumentProcessingService } from './document-processing.service';
-import { PremiumAvatarPipelineService } from './premium-avatar-pipeline.service';
 
 @Module({
   imports: [UploadModule],
-  providers: [DocumentProcessingService, PremiumAvatarPipelineService],
+  providers: [DocumentProcessingService],
   exports: [DocumentProcessingService],
 })
 export class DocumentProcessingModule {}
